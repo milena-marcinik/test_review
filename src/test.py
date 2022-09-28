53b485d82a12837c6b79733807318882be83d924
unknown <milena.marcinik@gmail.com>
2022-09-28 12:27:18 +0200

    Changes 2

--- src/test.py
+++ src/test.py
@@ -1,1 +1,15 @@
-print("test")
+import sys
+
+# print("test")
+# print("dupa dupa")
+
+
+def is_venv():
+    return (hasattr(sys, 'real_prefix') or
+            (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix))
+
+
+if is_venv():
+    print('inside virtualenv or venv')
+else:
+    print('outside virtualenv or venv')
